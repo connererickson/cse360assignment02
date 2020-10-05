d2e2a2c9f527baa1dfdadc13594136f55f142efd
cserick3 <cserick3@DESKTOP-LOB2EDB>
2020-10-05 14:20:00 -0700

    Added access information.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,6 +3,8 @@ package cse360assignment02;
 /**Description: The AddingMachine class is capable of adding and subtracting numbers from the total, which is initialized as 0.
  * Using the toString() method will show all recent transactions made after initialization or the clear() method is used.
  * GitHub Link: https://github.com/connererickson/cse360assignment02.git
+ * Access information: AddingMachine.java is in the src folder of the repository.
+ * 
  * @author Conner Erickson
  * @version 1.0
  *
