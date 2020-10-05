6edede4d80046574baf136971cca1f9e98cf3dd3
cserick3 <cserick3@DESKTOP-LOB2EDB>
2020-10-05 14:11:42 -0700

    Add simple implementations for several methods, as well as the repository link in the comment header.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,7 +1,8 @@
 package cse360assignment02;
 
-/**
- * 
+/**Description: The AddingMachine class is capable of adding and subtracting numbers from the total, which is initialized as 0.
+ * Using the toString() method will show all recent transactions made after initialization or the clear() method is used.
+ * GitHub Link: https://github.com/connererickson/cse360assignment02.git
  * @author Conner Erickson
  * @version 1.0
  *
@@ -9,25 +10,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String transactions = "";
   
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0;	// not needed - included for clarity
+    transactions += total;
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  transactions += " + " + value;
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  transactions += " - " + value;
   }
 
   public String toString () {
-    return "";
+    return transactions;
   }
 
   public void clear() {
+	  total = 0;
+	  transactions = "";
   }
 }
